0666f65554d688552761a1c24cb44d4ac707b639
six-my <six.my.2024@gmail.com>
2025-10-07 11:05:45 +0800

    fuwari接入twikoo

--- src/config.ts
+++ src/config.ts
@@ -85,3 +85,10 @@ export const expressiveCodeConfig: ExpressiveCodeConfig = {
 	// Please select a dark theme, as this blog theme currently only supports dark background color
 	theme: "github-dark",
 };
+
+export const commentConfig: CommentConfig = {
+  twikoo: {
+    envId: 'https://twikoofuwari.zeabur.app',
+    lang: 'zh-CN',
+  },
+}
